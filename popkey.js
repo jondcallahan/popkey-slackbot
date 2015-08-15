5164f3aa4d9e3b5372b92a652ced655047160907
Jon Callahan <jondcallahan@users.noreply.github.com>
2015-08-14 21:09:37 -0500

    seeing what slack sends

--- popkey.js
+++ popkey.js
@@ -3,6 +3,7 @@ module.exports = function (req, res, next) {
   var popSearchURL = 'http://popkey.co/search/' + encodeURI(userText);
   var cheerio = require('cheerio');
   var request = require('request');
+
   request(popSearchURL, function (error, response, html) {
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
@@ -14,9 +15,10 @@ module.exports = function (req, res, next) {
             gifArray.push(gifURL);
           });
           var randomGIF = gifArray[Math.floor(Math.random() * gifArray.length)];
-          return res.status(200).send(randomGIF);
+         // return res.status(200).send(randomGIF);
 
     }
+    return res.status(200).send(userText);
   });
 };
 
