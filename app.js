3b01f7ae958b081c79865e98e687372d854a2801
Jon Callahan <jondcallahan@users.noreply.github.com>
2015-08-14 22:11:28 -0500

    corrected instructions

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // get route if someone stumbles upon app page
- app.get('/', function (req, res) { res.status(200).send('<body style="background-color:azure; font-family: Helvetica;"><h2 style="margin-top:10%;"><hr style="border-color: tomato; width: 80%"><center>To use the popkey slack integration, add an outgoing webhook with the URL:<br /><br />' + req.get('host') + '/popkey</center><hr style="border-color: tomato; width: 80%"></h2></body>'); });
+ app.get('/', function (req, res) { res.status(200).send('<body style="background-color:azure; font-family: Helvetica;"><h2 style="margin-top:10%;"><hr style="border-color: tomato; width: 80%"><center>To use the popkey slack integration, add an outgoing webhook with the URL:<br /><br />' + req.get('host') + '/</center><hr style="border-color: tomato; width: 80%"></h2></body>'); });
 
 //post route for slack
 app.post('/', popkey );
