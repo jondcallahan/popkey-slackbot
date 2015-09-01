bf594dbf3e44df3166d28554f63e20857d2228de
Jon Callahan <jondcallahan@users.noreply.github.com>
2015-09-01 13:43:17 -0500

    experimenting with static file serving

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var popkey = require('./popkey');
-
+var url = require('url');
 var app = express();
 var port = process.env.PORT || 3000;
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//static file server middleware
+app.use(express.static('public'));
+
 // get route if someone stumbles upon app page
- app.get('/', function (req, res) { res.status(200).send('<body style="background-color:azure; font-family: Helvetica; display: flex; align-items: center; justify-content: center;"><h2><hr style="border-color: tomato; width: 80%"><center>To use the popkey slack integration, add an outgoing webhook with the URL:<br /><br />' + req.protocol + '://' + req.get('host') + '/</center><hr style="border-color: tomato; width: 80%"></h2></body>'); });
+ //app.get('/', function (req, res) { res.status(200).send('<body style="background-color:azure; font-family: Helvetica; display: flex; align-items: center; justify-content: center;"><h2><hr style="border-color: tomato; width: 80%"><center>To use the popkey slack integration, add an outgoing webhook with the URL:<br /><br />' + req.protocol + '://' + req.get('host') + '/</center><hr style="border-color: tomato; width: 80%"></h2></body>'); console.dir(url.Url('href'));});
 
 //post route for slack
 app.post('/', popkey );
